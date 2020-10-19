878a361ddc6bea212cdb29eea1f2e0ccc66ebe71
01gap <paola.pogli@gmail.com>
2020-10-19 18:02:00 +0300

    поправлена высота карточек круизов, добавлена проверка элементов js

--- build/js/main.js
+++ build/js/main.js
@@ -5,17 +5,19 @@
   const pageMain = document.querySelector('.page-main');
   const logo = document.querySelector('.page-header__logo');
 
-  mainNav.classList.remove('page-header__main-nav--no-js');
-  logo.classList.remove('page-header__logo--no-js');
-  pageMain.classList.remove('page-main--no-js');
+  if (mainNav !== null && navButton !== null && pageMain !== null && logo !== null) {
+    mainNav.classList.remove('page-header__main-nav--no-js');
+    logo.classList.remove('page-header__logo--no-js');
+    pageMain.classList.remove('page-main--no-js');
 
-  navButton.addEventListener('click', () => {
-    let expanded = navButton.getAttribute('aria-expanded') === 'true';
-    navButton.setAttribute('aria-expanded', !expanded);
-    mainNav.classList.toggle('page-header__main-nav--opened');
-    logo.classList.toggle('page-header__logo--menu-opened');
-    pageMain.classList.toggle('page-main--menu-opened');
-});
-})()
+    navButton.addEventListener('click', () => {
+      let expanded = navButton.getAttribute('aria-expanded') === 'true';
+      navButton.setAttribute('aria-expanded', !expanded);
+      mainNav.classList.toggle('page-header__main-nav--opened');
+      logo.classList.toggle('page-header__logo--menu-opened');
+      pageMain.classList.toggle('page-main--menu-opened');
+    });
+  }
+})();
 
 
