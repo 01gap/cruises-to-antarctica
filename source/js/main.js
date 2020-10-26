374d337783ab2aa492d1dd57bd163e5a11844926
01gap <paola.pogli@gmail.com>
2020-10-26 03:29:50 +0300

    правка пп

--- source/js/main.js
+++ source/js/main.js
@@ -1,21 +1,22 @@
 'use strict';
 (function() {
-  const mainNav = document.querySelector('.page-header__main-nav');
+  const pageHeader = document.querySelector('.page-header');
+  // const mainNav = document.querySelector('.page-header__main-nav');
   const navButton = mainNav.querySelector('.main-nav__toggle');
-  const pageMain = document.querySelector('.page-main');
-  const logo = document.querySelector('.page-header__logo');
+  // const pageMain = document.querySelector('.page-main');
+  // const logo = document.querySelector('.page-header__logo');
 
-  if (mainNav !== null && navButton !== null && pageMain !== null && logo !== null) {
-    mainNav.classList.remove('page-header__main-nav--no-js');
-    logo.classList.remove('page-header__logo--no-js');
-    pageMain.classList.remove('page-main--no-js');
+  if (pageHeader !== null && navButton !== null) {
+    pageHeader.classList.remove('page-header--no-js');
+    // logo.classList.remove('page-header__logo--no-js');
+    // pageMain.classList.remove('page-main--no-js');
 
     navButton.addEventListener('click', () => {
       let expanded = navButton.getAttribute('aria-expanded') === 'true';
       navButton.setAttribute('aria-expanded', !expanded);
-      mainNav.classList.toggle('page-header__main-nav--opened');
-      logo.classList.toggle('page-header__logo--menu-opened');
-      pageMain.classList.toggle('page-main--menu-opened');
+      pageHeader.classList.toggle('page-header--menu-opened');
+      // logo.classList.toggle('page-header__logo--menu-opened');
+      // pageMain.classList.toggle('page-main--menu-opened');
     });
   }
 })();
